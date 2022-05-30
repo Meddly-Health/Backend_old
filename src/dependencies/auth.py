4e5e1f3336254f253a530e9b09bc4e8779bf1fb5
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-05-30 18:04:47 -0300

    Added supervisors

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -1,7 +1,7 @@
 import datetime
 
 import firebase_admin
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, Header, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from firebase_admin import auth, credentials
 
@@ -13,9 +13,14 @@ firebase_admin.initialize_app(cred)
 
 
 async def authenticate(
-    cred: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
-    db=Depends(Database.get_db),
+        cred: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
+        db=Depends(Database.get_db),
 ):
+    """
+    Authenticate a user with a Bearer token.
+    This does not support the supervisor role.
+    """
+
     if cred is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -30,10 +35,10 @@ async def authenticate(
             detail=f"Invalid authentication credentials. {err}",
         )
 
-    user = await db["user"].find_one({"user_id": decoded_token["user_id"]})
+    user = await db["user"].find_one({"_id": decoded_token["user_id"]})
     if not user:
         user = {
-            "user_id": decoded_token["user_id"],
+            "_id": decoded_token["user_id"],
             "email": decoded_token["email"],
             "created_at": datetime.datetime.now(),
             "updated_at": datetime.datetime.now(),
@@ -42,4 +47,60 @@ async def authenticate(
             "supervised": [],
         }
         await db["user"].insert_one(user)
-    return decoded_token
+    return {"user_id": decoded_token["user_id"],
+            "email": decoded_token["email"]}
+
+
+async def authenticate_with_supervisor(
+        cred: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
+        db=Depends(Database.get_db), supervised_id: str | None = Header(default=None)
+):
+    """
+    Authenticate a user with a Bearer token.
+    This supports the supervisor role.
+    """
+    if cred is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Bearer authentication required",
+        )
+
+    try:
+        decoded_token = auth.verify_id_token(cred.credentials)
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials. {err}",
+        )
+
+    if supervised_id:
+        user = await db["user"].find_one({"_id": supervised_id})
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Supervised user does not exist",
+            )
+        if decoded_token["user_id"] not in user["supervisors"]:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User is not a supervisor of the supervised user",
+            )
+        return {"user_id": user["user_id"],
+                "email": user["email"]}
+
+    user = await db["user"].find_one({"_id": decoded_token["user_id"]})
+    if not user:
+        user = {
+            "_id": decoded_token["user_id"],
+            "email": decoded_token["email"],
+            "created_at": datetime.datetime.now(),
+            "updated_at": datetime.datetime.now(),
+            "diseases": [],
+            "supervisors": [],
+            "supervised": [],
+        }
+        await db["user"].insert_one(user)
+    return {"user_id": user["user_id"],
+            "email": user["email"]}
+
+
---
+++ src/routers/supervisors.py
@@ -0,0 +1,52 @@
+import datetime
+
+from fastapi import APIRouter, Depends
+from fastapi.encoders import jsonable_encoder
+
+from database import Database
+from dependencies import auth
+from schemas.user import UserModel, UserUpdateModel
+
+router = APIRouter(prefix="/supervisors", tags=["User"])
+
+
+@router.delete("/supervisor/{supervisor_id}", status_code=200, summary="Delete supervisor")
+async def delete_supervisor(
+        supervisor_id: str,
+        user=Depends(auth.authenticate),
+        db=Depends(Database.get_db),
+):
+    """
+    Elimina un supervisor
+    """
+    supervisor = await db.get_supervisor(supervisor_id)
+    supervised = await db.get_supervised(user["user_id"])
+
+    supervised["supervisors"].remove(supervisor_id)
+    supervisor["supervised"].remove(user["user_id"])
+
+    await db["user"].update_one({"_id": supervisor_id}, {"$set": supervisor})
+    await db["user"].update_one({"_id": user["user_id"]}, {"$set": supervised})
+
+    return {"status": "ok"}
+
+
+@router.delete("/supervised/{supervised_id}", status_code=200, summary="Delete supervisor")
+async def delete_supervised(
+        supervised_id: str,
+        user=Depends(auth.authenticate),
+        db=Depends(Database.get_db),
+):
+    """
+    Elimina un supervisor
+    """
+    supervisor = await db.get_supervisor(user["user_id"])
+    supervised = await db.get_supervised(supervised_id)
+
+    supervised["supervisors"].remove(user["user_id"])
+    supervisor["supervised"].remove(supervised_id)
+
+    await db["user"].update_one({"_id": user["user_id"]}, {"$set": supervisor})
+    await db["user"].update_one({"_id": supervised_id}, {"$set": supervised})
+
+    return {"status": "ok"}
