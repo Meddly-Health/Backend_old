09aefc7f556620c341a82cea691fef1b3e241913
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-06-01 00:50:12 -0300

    fix: 🐛 Included supervisors router

--- src/app.py
+++ src/app.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 import config
 from database import Database
-from routers import test, user
+from routers import test, user, supervisors
 
 app = FastAPI(**config.metadata)
 
 app.include_router(user.router)
+app.include_router(supervisors.router)
 app.include_router(test.router)
 
 app.add_event_handler("startup", Database.connect_db)
