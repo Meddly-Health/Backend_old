76f04da885b2f49bc874347878c19e6cb6e4c784
[Bot] Ignacio Pieve Roiger <ignacio.pieve@gmail.com>
2022-07-19 01:27:16 +0000

    style: 🎨 [Bot] Format Python code with Black

--- src/pruebas/pruebas_sg.py
+++ src/pruebas/pruebas_sg.py
@@ -10,14 +10,12 @@ from config import sendgrid_key
 
 
 def send_dynamic(template_id, send_to, template_data):
-    """ Send a dynamic email to a list of email addresses
+    """Send a dynamic email to a list of email addresses
 
     :returns API response code
-    :raises Exception e: raises an exception """
+    :raises Exception e: raises an exception"""
     # create Mail object and populate
-    message = Mail(
-        from_email='meddly.health@gmail.com',
-        to_emails=send_to)
+    message = Mail(from_email="meddly.health@gmail.com", to_emails=send_to)
     # pass custom values for our HTML placeholders
     message.dynamic_template_data = template_data
     message.template_id = template_id
@@ -31,5 +29,5 @@ def send_dynamic(template_id, send_to, template_data):
 
 
 if __name__ == "__main__":
-    send_to = [('soficibello@gmail.com', 'Ignacio Pieve')]
-    send_dynamic('d-a5ff5b76bb8443beb7e4192b6c6e6863', 'soficibello@gmail.com', {})
+    send_to = [("soficibello@gmail.com", "Ignacio Pieve")]
+    send_dynamic("d-a5ff5b76bb8443beb7e4192b6c6e6863", "soficibello@gmail.com", {})
