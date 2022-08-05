2c57eb30727ce9415db87af186607a8ab906b670
Ignacio Pieve <ignacio.pieve@gmail.com>
2022-08-05 20:17:40 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,24 +1,22 @@
 try:
     import set_environ
 except ModuleNotFoundError:
-    pass
+    print('set_environ.py not found, using system environment variables')
 
 from fastapi import FastAPI
 
 import config
 from database import Database
 from routers import medicine, supervisors, test, user
-from utils import LoggingMiddleware
 
 app = FastAPI(**config.metadata)
 
+# ----- ROUTERS -----
 app.include_router(user.router)
 app.include_router(supervisors.router)
 app.include_router(medicine.router)
 app.include_router(test.router)
 
-
+# ----- DATABASE -----
 app.add_event_handler("startup", Database.connect_db)
 app.add_event_handler("shutdown", Database.close_db)
-
-app.add_middleware(LoggingMiddleware)
